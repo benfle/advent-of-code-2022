64bd6b60b3456f92ca7a09485d5a42fd8efcf291
Benoît Fleury <me@benfle.com>
2022-12-10 20:50:48 -0500

    Day 8 - Part 2

--- src/day08.clj
+++ src/day08.clj
@@ -36,3 +36,32 @@
            (visible? [i j]))
          (filter identity)
          count)))
+
+(defn- directional-scenic-score
+  [max-height heights]
+  (loop [[hd & xs] heights
+         score     0]
+    (cond
+      (not hd)           score
+      (< hd max-height) (recur xs (inc score))
+      :else             (inc score))))
+
+(defn- four-views
+  [m [i j]]
+  [(reverse (map #(get-in m [% j]) (range 0 i)))
+   (map #(get-in m [i %]) (range (inc j) (count (first m))))
+   (map #(get-in m [% j]) (range (inc i) (count m)))
+   (reverse (map #(get-in m [i %]) (range 0 j)))])
+
+(defn scenic-score
+  [m [i j]]
+  (->> (four-views m [i j])
+       (map #(directional-scenic-score (get-in m [i j]) %))
+       (apply *)))
+
+(def answer2
+  (let [m (read-map)]
+    (->> (for [i (range (count m))
+               j (range (count (first m)))]
+           (scenic-score m [i j]))
+         (reduce max 0))))
